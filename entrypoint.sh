a4df355654b3830bce743dcda8c58b94133936b4
isaaccrum <71954498+isaaccrum@users.noreply.github.com>
2025-11-20 02:11:08 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,17 +5,17 @@ if ! id debian >/dev/null 2>&1; then
     groupadd --gid 1020 debian
     useradd --shell /bin/bash --uid 1020 --gid 1020 --groups sudo --password "$(openssl passwd debian)" --create-home --home-dir /home/debian debian
     touch /home/ubuntu/.bashrc
-    echo "alias vim=nvim" >> /home/ubuntu/.bashrc 
-    echo "alias v=nvim" >> /home/ubuntu/.bashrc
-    touch /home/ubuntu/.xsession
-    echo "exec i3" > /home/ubuntu/.xsession
+    echo "alias vim=nvim" >> /home/debian/.bashrc 
+    echo "alias v=nvim" >> /home/debian/.bashrc
+    touch /home/debian/.xsession
+    echo "exec i3" > /home/debian/.xsession
 
     # Install NvChad for VIM
-    # git clone https://github.com/LazyVim/starter /home/ubuntu/.config/nvim --this is for Lazyvim; we are using NVChad instead.
-    mv /home/ubuntu/.config/nvim{,.bak}
-    # git clone -b v2.5 https://github.com/NvChad/NvChad /home/ubuntu/.config/nvim --depth 1
-    git clone https://github.com/NvChad/NvChad /home/ubuntu/.config/nvim --depth 1
-    rm -rf /home/ubuntu/.config/nvim/.git
+    # git clone https://github.com/LazyVim/starter /home/debian/.config/nvim --this is for Lazyvim; we are using NVChad instead.
+    mv /home/debian/.config/nvim{,.bak}
+    # git clone -b v2.5 https://github.com/NvChad/NvChad /home/debian/.config/nvim --depth 1
+    git clone https://github.com/NvChad/NvChad /home/debian/.config/nvim --depth 1
+    rm -rf /home/debian/.config/nvim/.git
     
     # Install Nerd Fonts
     mkdir -p /usr/local/share/fonts/jetbrains && curl -L -o /usr/local/share/fonts/jetbrains/jetbrains.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
@@ -37,4 +37,3 @@ else
     /usr/sbin/xrdp
     exec "$@"
 fi
-
