8c0f7eec142beb90a97d473894d4707835bfb5f9
isaaccrum <71954498+isaaccrum@users.noreply.github.com>
2025-11-20 01:43:13 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,18 @@ if ! id debian >/dev/null 2>&1; then
     echo "alias vim=nvim" >> /home/ubuntu/.bashrc 
     echo "alias v=nvim" >> /home/ubuntu/.bashrc
     echo "exec i3" > /home/ubuntu/.xsession
+
+    # Install NvChad for VIM
+    # git clone https://github.com/LazyVim/starter /home/ubuntu/.config/nvim --this is for Lazyvim; we are using NVChad instead.
+    mv /home/ubuntu/.config/nvim{,.bak}
+    # git clone -b v2.5 https://github.com/NvChad/NvChad /home/ubuntu/.config/nvim --depth 1
+    git clone https://github.com/NvChad/NvChad /home/ubuntu/.config/nvim --depth 1
+    rm -rf /home/ubuntu/.config/nvim/.git
+    
+    # Install Nerd Fonts
+    mkdir -p /usr/local/share/fonts/jetbrains && curl -L -o /usr/local/share/fonts/jetbrains/jetbrains.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/JetBrainsMono.zip
+    unzip /usr/share/local/share/fonts/jetbrains.zip
+    fc-cache -fv
 fi
 
 # Remove existing sesman/xrdp PID files to prevent rdp sessions hanging on container restart
