b78196d9b80a4daf3a47c157e3a76d5bfde741fb
isaaccrum <71954498+isaaccrum@users.noreply.github.com>
2025-11-20 01:40:07 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@
 if ! id debian >/dev/null 2>&1; then
     groupadd --gid 1020 debian
     useradd --shell /bin/bash --uid 1020 --gid 1020 --groups sudo --password "$(openssl passwd debian)" --create-home --home-dir /home/debian debian
+    echo "alias vim=nvim" >> /home/ubuntu/.bashrc 
+    echo "alias v=nvim" >> /home/ubuntu/.bashrc
+    echo "exec i3" > /home/ubuntu/.xsession
 fi
 
 # Remove existing sesman/xrdp PID files to prevent rdp sessions hanging on container restart
@@ -20,3 +23,4 @@ else
     /usr/sbin/xrdp
     exec "$@"
 fi
+
